621391172bef2ac3a4d4a301f0cad450b4e3b8c8
Troy Leach <troyleach29@gmail.com>
2016-03-15 22:46:31 -0500

    project 9 and 10

--- pythagorean_triplet.rb
+++ pythagorean_triplet.rb
@@ -23,21 +23,3 @@ def is_pythagorean_trplet?
   end_time = Time.now
   puts "This method took #{(end_time - start_time) * 1000} milliseconds"
 end
-
-def is_pythagorean_trplet?
-  start_time = Time.now
-  (1..500).each do |a|
-    (1..500).each do |b|
-      c = Math.sqrt( (a * a) + (b * b) ).to_i
-      if is_square?( c )
-        sum = a + b + c
-          if sum == 1000
-            puts  "#{a} + #{b} + #{c} = #{sum}"
-            break
-          end
-      end
-    end
-  end
-  end_time = Time.now
-  puts "This method took #{(end_time - start_time) * 1000} milliseconds"
-end
---
+++ sum_primes.rb
@@ -0,0 +1,28 @@
+#problem 10
+
+
+def is_prime?(number)
+  (2..(Math.sqrt(number).to_i)).to_a.each do |num|
+    return false if number % num == 0
+  end
+  true
+end
+
+def find_this2(max)
+  start_time = Time.now
+  num = 3
+  results = [2]
+  while results.last <= max
+    results << num if is_prime?(num)
+    num = num + 2
+  end
+  results.pop
+  results = results.inject(:+)
+  end_time = Time.now
+  puts "This took #{(end_time - start_time) * 1000} milliseconds"
+  return results
+end
+
+def sum_of_primes_below(number)
+  
+end
